1d554e3a80a4ca5e5932b39ab365f3b6c6b47206
PeterAsenov22 <peter12345@abv.bg>
2019-04-04 00:52:09 +0300

    add Signing Requests navigation link to navbar component

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SigningService } from 'src/app/core/services/signing/signing.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  public pendingRequestsCount = 0;
+
+  constructor(private signingService: SigningService) { }
+
+  ngOnInit() {
+    chrome.browserAction.getBadgeText({}, (result) => {
+      this.pendingRequestsCount = parseInt(result, 10);
+    });
+
+    this.signingService.change.subscribe(pendingRequestsCount => {
+      this.pendingRequestsCount = pendingRequestsCount;
+    });
+  }
 }
