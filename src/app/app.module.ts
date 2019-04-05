576f5793ba6cde578696bdc189a514b317cbee21
PeterAsenov22 <peter12345@abv.bg>
2019-04-05 13:04:46 +0300

    add highlight.js for signer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { DialogsModule } from './components/dialogs/dialogs.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GuardsModule } from './core/guards/guards.module';
+import { HighlightJsModule, HIGHLIGHT_JS } from 'angular-highlight-js';
+import hljs from 'highlight.js/lib/highlight';
+import json from 'highlight.js/lib/languages/json';
 import { NgModule } from '@angular/core';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ServicesModule } from './core/services/services.module';
@@ -14,6 +17,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { SignerComponent } from './components/signer/signer.component';
+
+hljs.registerLanguage('json', json);
+
+export function highlightJsFactory() {
+  return hljs;
+}
 
 @NgModule({
   declarations: [
@@ -29,6 +38,10 @@ import { SignerComponent } from './components/signer/signer.component';
     DialogsModule,
     FormsModule,
     GuardsModule,
+    HighlightJsModule.forRoot({
+      provide: HIGHLIGHT_JS,
+      useFactory: highlightJsFactory
+    }),
     NgxSpinnerModule,
     ReactiveFormsModule,
     ServicesModule,
