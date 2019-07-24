8baf6a0782610395ae48138178f111b2c0471ec8
Peter Asenov <peter12345@abv.bg>
2019-07-24 19:05:43 +0300

    add manage-dids component

--- src/app/components/did/index.ts
+++ src/app/components/did/index.ts
@@ -1,13 +1,15 @@
 import { AuthenticationKeysComponent } from './authentication-keys/authentication-keys.component';
 import { EncryptKeysComponent } from './encrypt-keys/encrypt-keys.component';
+import { ManageDidsComponent } from './manage-dids/manage-dids.component';
 import { ProvideDidComponent } from './provide-did/provide-did.component';
 import { PublicKeysComponent } from './public-keys/public-keys.component';
 import { ServicesComponent } from './services/services.component';
 import { SummaryComponent } from './summary/summary.component';
 
-export const createComponents = [
+export const didComponents = [
   AuthenticationKeysComponent,
   EncryptKeysComponent,
+  ManageDidsComponent,
   ProvideDidComponent,
   PublicKeysComponent,
   ServicesComponent,
