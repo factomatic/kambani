2747adcac071710d3756402ecdf0868e30ce033b
Peter Asenov <peter12345@abv.bg>
2019-07-24 18:58:15 +0300

    remove action component

--- src/app/components/shared/index.ts
+++ src/app/components/shared/index.ts
@@ -1,9 +1,7 @@
-import { ActionComponent } from './action/action.component';
 import { FinalComponent } from './final/final.component';
 import { NavbarComponent } from './navbar2/navbar.component';
 
 export const sharedComponents = [
-  ActionComponent,
   FinalComponent,
   NavbarComponent
 ];
