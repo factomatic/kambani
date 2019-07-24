bf19d265680ea698ff1d78fc0d316fd84785fe76
Peter Asenov <peter12345@abv.bg>
2019-07-24 13:02:32 +0300

    order imports in app.module.ts and keep separation between internal and external imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,44 @@
-import { AppRoutingModule } from './app-routing.module';
+import { ActionReducer, StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { DialogsModule } from './components/dialogs/dialogs.module';
+import { DeviceDetectorModule } from 'ngx-device-detector';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { GuardsModule } from './core/guards/guards.module';
 import { HighlightJsModule, HIGHLIGHT_JS } from 'angular-highlight-js';
 import hljs from 'highlight.js/lib/highlight';
-import json from 'highlight.js/lib/languages/json';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import json from 'highlight.js/lib/languages/json';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NgModule } from '@angular/core';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { DeviceDetectorModule } from 'ngx-device-detector';
-import { ServicesModule } from './core/services/services.module';
-import { ToastrModule } from 'ngx-toastr';
-import { VaultModule } from './components/vault/vault.module';
-import { AppState } from './core/store/app.state';
-import { environment } from '../environments/environment';
 import { storeLogger } from 'ngrx-store-logger';
-import { ErrorInterceptor } from './core/interceptors/error.interceptor';
-import { SharedModule } from './components/shared/shared.module';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { appReducers } from './core/store/app.reducers';
+import { AppRoutingModule } from './app-routing.module';
+import { AppState } from './core/store/app.state';
+import { DialogsModule } from './components/dialogs/dialogs.module';
+import { environment } from '../environments/environment';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { GuardsModule } from './core/guards/guards.module';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ServicesModule } from './core/services/services.module';
+import { SharedModule } from './components/shared/shared.module';
 import { SignerComponent } from './components/signer/signer.component';
-import { ActionReducer, StoreModule } from '@ngrx/store';
-import { reducers } from './reducers';
+import { VaultModule } from './components/vault/vault.module';
 
 hljs.registerLanguage('json', json);
+
+export function highlightJsFactory() {
+  return hljs;
+}
 
 export function logger(reducer: ActionReducer<AppState>): any {
   return storeLogger()(reducer);
 }
 
 export const metaReducers = environment.production || environment.staging ? [] : [logger];
-
-export function highlightJsFactory() {
-  return hljs;
-}
 
 @NgModule({
   declarations: [
@@ -52,6 +51,7 @@ export function highlightJsFactory() {
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    DeviceDetectorModule.forRoot(),
     DialogsModule,
     FormsModule,
     GuardsModule,
@@ -59,16 +59,15 @@ export function highlightJsFactory() {
       provide: HIGHLIGHT_JS,
       useFactory: highlightJsFactory
     }),
-    MDBBootstrapModule.forRoot(),
     HttpClientModule,
+    MDBBootstrapModule.forRoot(),
     NgxSpinnerModule,
-    DeviceDetectorModule.forRoot(),
     ReactiveFormsModule,
-    SharedModule,
     ServicesModule,
+    SharedModule,
+    StoreModule.forRoot(appReducers, { metaReducers }),
     ToastrModule.forRoot(),
-    VaultModule,
-    StoreModule.forRoot(appReducers, { metaReducers })
+    VaultModule
   ],
   providers: [
     {
