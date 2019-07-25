a3193dfd4be29a9046e3cc23b2850cad8ae017dc
Peter Asenov <peter12345@abv.bg>
2019-07-25 12:30:40 +0300

    add versions to environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   storageKey: '@#signerextension#@',
   staging: false,
-  version: '1.0',
+  entrySchemaVersion: '1.0.0',
+  didMethodSpecVersion: '0.1.0',
   entrySizeLimit: 10275,
   apiUrl: 'https://testnet-api.factomatic.io/write-did'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   storageKey: '@#signerextension#@',
   staging: false,
-  version: '1.0',
+  entrySchemaVersion: '1.0.0',
+  didMethodSpecVersion: '0.1.0',
   entrySizeLimit: 10275,
   apiUrl: 'https://testnet-api.factomatic.io/write-did'
 };
