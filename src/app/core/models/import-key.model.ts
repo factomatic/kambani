f39f457027a3b85d5e17fd5e8805243b80bae3dc
Peter Asenov <peter12345@abv.bg>
2019-09-14 15:57:55 +0300

    remove deprecated methods and models

--- src/app/core/models/import-key.model.ts
+++
@@ -1,8 +0,0 @@
-export class ImportKeyModel {
-  constructor(
-    public alias: string,
-    public type: string,
-    public publicKey: string,
-    public privateKey: string
-  ) { }
-}
--- src/app/core/models/import-result.model.ts
+++
@@ -1,6 +0,0 @@
-export class ImportResultModel {
-  constructor(
-    public success: boolean,
-    public message: string
-  ) { }
-}
