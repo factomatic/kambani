5d879f2a4abfa24dc5cf0e22412d9b1131432efd
Peter Asenov <peter12345@abv.bg>
2019-09-17 13:59:31 +0300

    integrate home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,34 +1,26 @@
-import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-
-import { BackupResultModel } from 'src/app/core/models/backup-result.model';
-import { DialogsService } from 'src/app/core/services/dialogs/dialogs.service';
-import { downloadFile } from 'src/app/core/utils/helpers';
-import { ModalSizeTypes } from 'src/app/core/enums/modal-size-types';
-import { PasswordDialogComponent } from '../dialogs/password/password.dialog.component';
-import { VaultService } from 'src/app/core/services/vault/vault.service';
 import { Chart } from 'chart.js';
+import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
+
+import { VaultService } from 'src/app/core/services/vault/vault.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-
-
-
 export class HomeComponent implements OnInit {
-  public didIds: string[] = [];
+  public pendingRequestsCount: number = 0;
+  public signedRequestsCount: number = 0;
+  public didsCount: number;
   
-
-  constructor(
-    private dialogsService: DialogsService,
-    private toastr: ToastrService,
-    private vaultService: VaultService) { }
+  constructor(private vaultService: VaultService) { }
 
   ngOnInit() {
-    const didDocuments = this.vaultService.getAllDIDDocuments();
-    this.didIds = Object.keys(didDocuments);
+    this.didsCount = this.vaultService.getDIDsCount();
+    
+    chrome.browserAction.getBadgeText({}, (result) => {
+      this.pendingRequestsCount = parseInt(result, 10);
+    });
   }
 
   @ViewChild('signingRequests') chart: ElementRef;
@@ -110,42 +102,5 @@ export class HomeComponent implements OnInit {
             }
         });
 
-  }
-
-  backupDid(didId: string) {
-    const dialogMessage = 'Enter your vault password to open the vault and encrypt your DID';
-
-    this.dialogsService.open(PasswordDialogComponent, ModalSizeTypes.ExtraExtraLarge, dialogMessage)
-      .subscribe((vaultPassword: string) => {
-        if (vaultPassword) {
-          this.vaultService
-            .backupSingleDIDFromVault(didId, vaultPassword)
-            .subscribe((result: BackupResultModel) => {
-              if (result.success) {
-                const date = new Date();
-                const didBackupFile = this.postProcessDidBackupFile(result.backup, didId);
-                downloadFile(didBackupFile, `paper-did-UTC--${date.toISOString()}.txt`);
-              } else {
-                this.toastr.error(result.message);
-              }
-            });
-        }
-      });
-  }
-
-  private postProcessDidBackupFile(encryptedFile: string, didId: string) {
-    const parsedFile = JSON.parse(encryptedFile);
-    const newKeysFile: any = { };
-
-    newKeysFile.data = parsedFile.data;
-    newKeysFile.encryptionAlgo = {
-      name: 'AES-GCM',
-      iv: parsedFile.iv,
-      salt: parsedFile.salt,
-      tagLength: 128
-    };
-    newKeysFile.did = didId;
-
-    return JSON.stringify(newKeysFile, null, 2);
   }
 }
