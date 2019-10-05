18f6fbe47b79fa78b820086b7bef904ef7404636
Peter Asenov <peter12345@abv.bg>
2019-10-05 13:18:12 +0300

    update dialogs components

--- src/app/components/dialogs/remove-vault/remove-vault.dialog.component.ts
+++ src/app/components/dialogs/remove-vault/remove-vault.dialog.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  templateUrl: './remove-vault.dialog.component.html'
+  templateUrl: './remove-vault.dialog.component.html',
+  styleUrls: ['./remove-vault.dialog.component.scss']
 })
 
 export class RemoveVaultDialogComponent {
