33fb92fa92d74290f1665e5ac28bd909fac4f6b1
Peter Asenov <peter12345@abv.bg>
2019-11-09 21:56:11 +0200

    update fct and ec addresses requests handlers

--- src/factomAddressesContentScript.js
+++ src/factomAddressesContentScript.js
@@ -1,10 +1,30 @@
-window.addEventListener('GetFactomAddresses', (event) => {
+window.addEventListener('GetAllAddresses', (event) => {
   chrome.storage.sync.get(['fctAddresses', 'ecAddresses'], function(result) {
-    var event = new CustomEvent('ReceiveFactomAddresses', {
+    const event = new CustomEvent('AllAddresses', {
       detail: {
-        'fctAddresses': result.fctAddresses,
-        'ecAddresses': result.ecAddresses
+        "fct": result.fctAddresses,
+        "ec": result.ecAddresses
       }
+    });
+
+    window.dispatchEvent(event);
+  });
+});
+
+window.addEventListener('GetFCTAddresses', (event) => {
+  chrome.storage.sync.get(['fctAddresses'], function(result) {
+    const event = new CustomEvent('FCTAddresses', {
+      detail: result.fctAddresses
+    });
+
+    window.dispatchEvent(event);
+  });
+});
+
+window.addEventListener('GetECAddresses', (event) => {
+  chrome.storage.sync.get(['ecAddresses'], function(result) {
+    const event = new CustomEvent('ECAddresses', {
+      detail: result.ecAddresses
     });
 
     window.dispatchEvent(event);
@@ -12,19 +32,56 @@ window.addEventListener('GetFactomAddresses', (event) => {
 });
   
 chrome.storage.onChanged.addListener(function(changes) {
-  let changedAddresses = {};
   if (changes.fctAddresses) {
-    changedAddresses['fctAddresses'] = changes.fctAddresses.newValue;
+    const fctAddressesOldValue = changes.fctAddresses.oldValue;
+    const fctAddressesNewValue = changes.fctAddresses.newValue;
+
+    if (fctAddressesOldValue.length < fctAddressesNewValue.length) {
+      const addedFctAddress = fctAddressesNewValue.filter(addr => !fctAddressesOldValue.includes(addr))[0];
+
+      const event = new CustomEvent('FCTAddressesChanged', {
+        detail: {
+          "added": addedFctAddress
+        }
+      });
+
+      window.dispatchEvent(event);
+    } else {
+      const removedFctAddress = fctAddressesOldValue.filter(addr => !fctAddressesNewValue.includes(addr))[0];
+
+      const event = new CustomEvent('FCTAddressesChanged', {
+        detail: {
+          "removed": removedFctAddress
+        }
+      });
+
+      window.dispatchEvent(event);
+    }
+  } else if (changes.ecAddresses) {
+    const ecAddressesOldValue = changes.ecAddresses.oldValue;
+    const ecAddressesNewValue = changes.ecAddresses.newValue;
+
+    if (ecAddressesOldValue.length < ecAddressesNewValue.length) {
+      const addedEcAddress = ecAddressesNewValue.filter(addr => !ecAddressesOldValue.includes(addr))[0];
+
+      const event = new CustomEvent('ECAddressesChanged', {
+        detail: {
+          "added": addedEcAddress
+        }
+      });
+
+      window.dispatchEvent(event);
+    } else {
+      const removedEcAddress = ecAddressesOldValue.filter(addr => !ecAddressesNewValue.includes(addr))[0];
+
+      const event = new CustomEvent('ECAddressesChanged', {
+        detail: {
+          "removed": removedEcAddress
+        }
+      });
+
+      window.dispatchEvent(event);
+    }
   }
-
-  if (changes.ecAddresses) {
-    changedAddresses['ecAddresses'] = changes.ecAddresses.newValue;
-  }
-
-  var event = new CustomEvent('ReceiveFactomAddresses', {
-    detail: changedAddresses
-  });
-
-  window.dispatchEvent(event);
 });
   
