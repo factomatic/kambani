4432daa255d1dedccb941f6c3170d6ba84380ced
Peter Asenov <peter12345@abv.bg>
2019-11-26 19:38:31 +0200

    rename and update form state

--- src/app/core/store/create-did/create-did.state.ts
+++ src/app/core/store/create-did/create-did.state.ts
@@ -2,11 +2,8 @@ import { DidKeyModel } from '../../models/did-key.model';
 import { ManagementKeyModel } from '../../models/management-key.model';
 import { ServiceModel } from '../../models/service.model';
 
-export interface FormState {
+export interface CreateDIDState {
   readonly managementKeys: ManagementKeyModel[];
   readonly didKeys: DidKeyModel[];
   readonly services: ServiceModel[];
-  readonly originalManagementKeys: ManagementKeyModel[];
-  readonly originalDidKeys: DidKeyModel[];
-  readonly originalServices: ServiceModel[];
 }
