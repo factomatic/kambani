053aec3f5832bc2bdc0aee3b33a14d73c88cc143
Peter Asenov <peter12345@abv.bg>
2020-01-21 16:44:41 +0200

    remove signerContentScript.js and factomAddressesContentScript.js

--- src/signerContentScript.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('SigningRequest', (event) => {
-  chrome.runtime.sendMessage({type: 'receiveSigningRequest', content: event.detail},
-    function(response) {
-      var event = new CustomEvent('SigningResponse', {detail: response});
-      window.dispatchEvent(event);
-    }
-  );
-});
