5f62e66883acab9b5300bf9eae131f38b84eedc0
Peter Asenov <peter12345@abv.bg>
2020-01-21 16:48:26 +0200

    bump localStorageVersion to 1.1

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,7 +4,7 @@ export const environment = {
   staging: true,
   entrySchemaVersion: '1.0.0',
   didMethodSpecVersion: '0.2.0',
-  localStorageVersion: '1.0',
+  localStorageVersion: '1.1',
   entrySizeLimit: 10275,
   apiUrl: 'https://testnet-api.factomatic.io/write-did'
 };
